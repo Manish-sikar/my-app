7d1a7f3ba51acdc57a28b6cf6fefc68e54c6b4f3
swatikulshreshtha <kulshreshtha9853@gmail.com>
2025-11-19 08:53:43 +0530

    Update Schedule.jsx

--- src/Herosection/Schedule.jsx
+++ src/Herosection/Schedule.jsx
@@ -5,47 +5,46 @@ const Schedule = () => {
     {
       time: "08:30 – 08:45 AM",
       title: "Reporting Time",
-      // desc: "Students report at the Registration Desk at the RIC Centre, outside Mini Audi 2.",
     },
     {
       time: "08:45 – 09:00 AM",
-      title: "Inaugural Session ",
+      title: "Inaugural Session",
       desc: "Students settle down inside the auditorium. The heads of each school will ensure that students are seated quietly.",
     },
     {
       time: "09:00 – 09:02 AM",
       title: "Introductory",
-      desc: "Opening Remarks by Ms.Renu Singh",
+      desc: "Opening Remarks by Ms. Renu Singh",
     },
     {
       time: "09:02 – 09:05 AM",
       title: "Opening Remarks",
-      desc: "Ms.Apra Kuchhal , Mr.Sandeep Sethi , shares about Saanjhi – The Mental Health Festival",
+      desc: "Ms. Apra Kuchhal & Mr. Sandeep Sethi share about Saanjhi – The Mental Health Festival",
     },
     {
       time: "09:05 – 09:10 AM",
       title: "Lamp Lighting & Prayer",
-      desc: "Lamp lighting, Lamp lighting Mr.Gajendra Singh, Pt.Vishwa Mohan Bhatt, Ms.Apra Kuchhal, Mr.Sandeep Sethi, Ms.Renu Singh.",
+      desc: "Lamp lighting by Mr. Gajendra Singh, Pt. Vishwa Mohan Bhatt, Ms. Apra Kuchhal, Mr. Sandeep Sethi, Ms. Renu Singh.",
     },
     {
       time: "09:10 – 09:20 AM",
       title: "Stage Performance",
-      desc: "Registration on mental health and well-being for students of IIS School, Sitapura,
+      desc: "Registration on mental health and well-being for students of IIS School, Sitapura",
     },
     {
       time: "09:20 – 09:50 AM",
       title: "Panel Discussion",
-      desc: "Panel Discussion - Opening remarks by Mr.Sandeep Sethi,
+      desc: "Panel Discussion - Opening remarks by Mr. Sandeep Sethi",
     },
     {
       time: "10:00 – 11:00 AM",
       title: "Motivational Talk",
-      desc: 'Talk by Mr. Ashwin Sanghi – “Power of Failure,
+      desc: "Talk by Mr. Ashwin Sanghi – “Power of Failure”",
     },
     {
       time: "11:15 – 12:15 PM",
       title: "Stress & Anxiety Management",
-      desc: "Session on “Tools to Manage Stress & Anxiety” by Ms. Riri Trivedi,
+      desc: "Session on “Tools to Manage Stress & Anxiety” by Ms. Riri Trivedi",
     },
     {
       time: "12:20 PM",
@@ -63,22 +62,21 @@ const Schedule = () => {
     {
       time: "08:30 – 08:45 AM",
       title: "Welcome Tea & Registration",
-      // desc: "Students report at the Registration Desk at the RIC Centre, outside Mini Audi 2.",
     },
     {
       time: "08:45 – 09:00 AM",
-      title: "Auditorium ",
+      title: "Auditorium",
       desc: "Students settle down inside the auditorium. The heads of each school will ensure that students are seated quietly.",
     },
     {
       time: "09:00 – 09:02 AM",
       title: "Opening Remarks",
-      desc: "Opening Remarks by Ms.Renu Singh",
+      desc: "Opening Remarks by Ms. Renu Singh",
     },
     {
       time: "09:02 – 09:05 AM",
       title: "About",
-      desc: "Ms.Apra Kuchhal, shares about Saanjhi – The Mental Health Festival",
+      desc: "Ms. Apra Kuchhal shares about Saanjhi – The Mental Health Festival",
     },
     {
       time: "09:05 – 09:10 AM",
@@ -93,7 +91,7 @@ const Schedule = () => {
     {
       time: "09:20 – 09:27 AM",
       title: "Dance Performance",
-      desc: "Dance performance by students of Palace Orchestra Tagore School",
+      desc: "Dance performance by students of Palace School (Tagore Wing)",
     },
     {
       time: "09:30 – 10:30 AM",
@@ -103,7 +101,7 @@ const Schedule = () => {
     {
       time: "10:30 – 10:40 AM",
       title: "Lamp Lighting",
-      desc: "Lamp lighting, Lamp lighting Mr.Gajendra Singh, Pt.Vishwa Mohan Bhatt, Ms.Apra Kuchhal, Mr.Sandeep Sethi, Ms.Renu Singh.",
+      desc: "Lamp lighting by Mr. Gajendra Singh, Pt. Vishwa Mohan Bhatt, Ms. Apra Kuchhal, Mr. Sandeep Sethi, Ms. Renu Singh.",
     },
     {
       time: "10:40 – 10:45 AM",
@@ -113,7 +111,7 @@ const Schedule = () => {
     {
       time: "10:45 – 11:30 AM",
       title: "Musical Talk",
-      desc: "Talk by Pandit Vishwa Mohan Bhatt on “The Creation of a Tune - Samanvay”, followed by a short demonstration",
+      desc: "Talk by Pandit Vishwa Mohan Bhatt on “The Creation of a Tune - Samanvay”, followed by a demonstration",
     },
     {
       time: "11:30 – 11:40 AM",
@@ -123,7 +121,7 @@ const Schedule = () => {
     {
       time: "11:50 AM – 12:00 Noon",
       title: "Song Performance",
-      desc: "Song by IIS, Shipra Path departure of students",
+      desc: "Song by IIS, Shipra Path",
     },
     {
       time: "12:30 PM",
